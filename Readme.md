098de5b366f4bc78d977e472902280c6199aa7db
kongoji6 <kongoji@gmail.com>
2015-03-30 22:11:02 +0200

    testem first setup

--- Readme.md
+++ Readme.md
@@ -12,3 +12,14 @@
 Rebuild after every change using:
 
     npm run-script build
+
+
+### test ###
+
+if on Windows, install [this fix](https://github.com/airportyh/testem#known-issues)
+
+    npm install https://github.com/airportyh/mocha/tarball/windowsfix
+
+Run tests with
+
+    npm test
---
+++ test/example.spec.js
@@ -0,0 +1,10 @@
+describe('TDD example testing', function () {
+  it('should bar', function () {
+    // This test will only run in Node.
+    expect(3).to.not.equal('three');
+  });
+  it('shold baz', function () {
+    // This test will run everywhere.
+    expect('foo').to.be.a('string');
+  });
+});
